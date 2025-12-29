4249ab0e11a7db569d97d8d1ee3a0bced0c93536
zhong li <1725874747@qq.com>
2025-12-29 11:31:52 +0800

    Time: 0 ms (100.00%) | Memory: 24.6 MB (17.15%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>vt;
+    void bianli(TreeNode* root){
+        if(root==nullptr)return ;
+        vt.push_back(root->val);
+        bianli(root->left);
+        bianli(root->right);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        //遍历树，将元素存入数组，数组排序，返回下标的值
+        bianli(root);
+        sort(vt.begin(),vt.end());
+        return vt[k-1];
+    }
+};
