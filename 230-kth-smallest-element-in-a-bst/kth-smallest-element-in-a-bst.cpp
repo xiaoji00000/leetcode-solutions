68193d2c1ad6abe379b8ea7f4c707570212705b9
zhong li <1725874747@qq.com>
2025-12-29 11:37:08 +0800

    Time: 0 ms (100.00%) | Memory: 24.5 MB (41.88%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -14,14 +14,13 @@ public:
     vector<int>vt;
     void bianli(TreeNode* root){
         if(root==nullptr)return ;
-        vt.push_back(root->val);
         bianli(root->left);
+        vt.push_back(root->val);
         bianli(root->right);
     }
     int kthSmallest(TreeNode* root, int k) {
         //遍历树，将元素存入数组，数组排序，返回下标的值
         bianli(root);
-        sort(vt.begin(),vt.end());
         return vt[k-1];
     }
 };
