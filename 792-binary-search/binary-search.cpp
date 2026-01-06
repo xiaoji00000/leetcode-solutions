a5391181a2ce8ae8ee5414873d3b4bcf0d5766e4
zhong li <1725874747@qq.com>
2026-01-06 14:26:29 +0800

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.31%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int length=nums.size();
-        for(int i=0;i<length;i++){
-            if(nums[i]==target)return i;
+        int length=nums.size()-1;
+        int left=0,right=length;
+        int index=(left+right)/2;
+        while(index!=left&&index!=right){
+            if(nums[index]==target)return index;
+            else if(nums[index]<target){
+                left=index;
+                index=(left+right)/2;
+            }
+            else{
+                right=index;
+                index=(left+right)/2;
+            }
         }
+        if(nums[left]==target)return left;
+        if(nums[right]==target)return right;
         return -1;
     }
 };
