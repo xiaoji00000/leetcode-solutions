b93f21600e7e0afeea41ae321365af20f52c81d5
zhong li <1725874747@qq.com>
2026-01-07 13:45:33 +0800

    Time: 5 ms (34.45%) | Memory: 30.2 MB (85.26%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int>vt;
-        for(const auto&n:nums){
-            vt.push_back(n*n);
+        for(auto&n:nums){
+            n=n*n;
         }
-        sort(vt.begin(),vt.end(),[](const auto&a,const auto&b){
+        sort(nums.begin(),nums.end(),[](const auto&a,const auto&b){
             return a<b;
         });
-        return vt;
+        return nums;
 
     }
 };
