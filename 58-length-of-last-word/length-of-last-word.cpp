5411b5135f3a00aa637bd47683650880de97f5df
zhong li <1725874747@qq.com>
2026-01-08 15:22:30 +0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.78%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int right=s.size()-1;
-        bool is_starte=false;
         while(right>=0&&s[right]==' '){
             right--;
         }
-        if(right>=0)is_starte=true;
         int ans=0;
         while(right>=0&&s[right--]!=' '){
             ans++;
